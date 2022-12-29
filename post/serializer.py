2911846199a61484abd1788438d66e67723a2830
stas <stas37951@gmail.com>
2022-12-29 12:59:28 +0200

    add register, login, logout

--- post/serializer.py
+++ post/serializer.py
@@ -1,5 +1,29 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Post
+
+
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class PostSerializer(serializers.ModelSerializer):
--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .api import PostCreateApi, PostApi, PostUpdateApi, PostDeleteApi
+from knox import views as knox_views
+from .api import PostCreateApi, PostApi, PostUpdateApi, PostDeleteApi, RegisterAPI, LoginAPI
 
 urlpatterns = [
+    path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
     path('api/create', PostCreateApi.as_view()),
     path('api/', PostApi.as_view()),
     path('api/<int:pk>', PostUpdateApi.as_view()),
